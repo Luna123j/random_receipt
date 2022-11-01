833ea7a4b51acd415c79514ab3347169889877bb
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-31 21:30:19 -0400

    change view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("hello")
+    template = loader.get_template('polls/index.html')
+    # food = {'name1': "sandwitch",
+    #         'name2': 'chicken',
+    #         'name3': 'ham',
+    #         'name4': 'beef',
+    #         'name5': 'pork',
+    #         'name6': 'vegetable',
+    #         'name7': 'fish',
+    #         'name8': 'seaffood',
+    #         }
+
+    food = ["sandwitch",'chicken','ham','beef','pork','vegetable','fish','seaffood']
+    return HttpResponse(template.render({"food": food},request))
