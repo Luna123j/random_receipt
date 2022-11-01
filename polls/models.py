aa22ff7f7aa78fd5bba32b04374887fc9c8519bf
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-31 20:30:35 -0400

    make migrate

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,14 @@
+from dis import Instruction
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+  name = models.CharField(max_length=200)
+  ingredient1 = models.TextField(default='')
+  ingredient2 = models.TextField(default='')
+  ingredient3 = models.TextField(default='')
+  ingredient4 = models.TextField(default='')
+  ingredient5 = models.TextField(default='')
+  ingredient6 = models.TextField(default='')
+  instruction = models.TextField(default='')
+  def __str__(self):
+        return self.name
